18956525c170743d772d12c3d72ffb9abbe1223b
JoseMitchel <55783174+JoseMitchel@users.noreply.github.com>
2019-09-25 11:18:43 +0300

    Add files via upload

---
+++ sale.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />	
+	<title>Акции | Интернет-магазин "Ортоп"</title>
+	<meta name="description" content='Покупайте вигодно с нашими акциями'/>
+	<link type="text/css" rel="stylesheet" media="all" href="css/blue2.css" />
+	<link type="text/css" rel="stylesheet" media="all" href="js/prettyPhoto/css/prettyPhoto.css" />
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<?php include "header.php"; ?>
+
+	
+	<!-- PAGE TITLE -->
+	<div id="cta">
+		<div id="cta_inner">
+			<h1>Акции</h1>
+		</div>
+		<div class="shadow_bottom"></div>
+	</div>
+	
+	<!-- BLOG -->
+	<div id="blog">
+		<div class="container_12">
+			<div class="grid_10">
+				<!-- post 1 -->
+				<div class="post first_post">
+					<div class="post_image">
+						<!-- Simple Slideshow -->
+						<ul class="dm3_slider" style="width: 100%; height: 260px;">
+							<li class="slide" data-enlargeurl="content/enlarged/1.jpg">
+								<img src="images/sale4.png"  alt="акция" title="акция" />
+							</li>
+						</ul>
+					</div>
+					
+					<h2><a href="contact.php">Новый Магазин? -10%</a></h2>
+					
+					<div class="post_meta">
+						<span class="entry_date">24 сентября - 12 октября 2019 года</span>
+					</div>
+					
+					<p>
+					В честь открытия Нового магазина по адресу проспект Маяковского 91В объявляем АКЦИЮ -10% на товары
+					</p>
+					
+					<p>
+						<a class="button" href="contact.php">Узнать больше<span></span></a>
+					</p>
+				</div>
+				
+				<!-- post 2 -->
+				<div class="post first_post">
+					<div class="post_image">
+						<!-- Simple Slideshow -->
+						<ul class="dm3_slider" style="width: 100%; height: 260px;">
+							<li class="slide" data-enlargeurl="content/enlarged/1.jpg">
+								<img src="images/sale4.png"  alt="акция" title="акция" />
+							</li>
+						</ul>
+					</div>
+					
+					<h2><a href="contact.php">ТМ SpaGel -30%</a></h2>
+					
+					<div class="post_meta">
+						<span class="entry_date">24 сентября - 22 октября 2019 года</span>
+					</div>
+					
+					<p>
+					Грандиозная скидка 30%!!!При покупке набора ТМ SpaGel "носочки + перчатки", Вы получаете скидку в размере 30%.
+					</p>
+					
+					<p>
+						<a class="button" href="contact.php">Узнать больше<span></span></a>
+					</p>
+				</div>
+				
+				<!-- post 3 -->
+				<div class="post first_post">
+					<div class="post_image">
+						<!-- Simple Slideshow -->
+						<ul class="dm3_slider" style="width: 100%; height: 260px;">
+							<li class="slide" data-enlargeurl="content/enlarged/1.jpg">
+								<img src="images/sale4.png" alt="акция" title="акция" />
+							</li>
+						</ul>
+					</div>
+					
+					<h2><a href="contact.php">Подарочные сертификаты</a></h2>
+					
+					<div class="post_meta">
+						<span class="entry_date">24 сентября - 31 декабря 2019 года</span>
+					</div>
+					
+					<p>
+					Подарочные сертификаты на сумму 300, 500 и 1000 грн, - это замечательный подарок. С подарочным сертификатом можно приобрести любой товар.
+					</p>
+					
+					<p>
+						<a class="button" href="contact.php">Узнать больше<span></span></a>
+					</p>
+				</div>
+				
+				<!-- post 4 -->
+				<div class="post first_post">
+					<div class="post_image">
+						<!-- Simple Slideshow -->
+						<ul class="dm3_slider" style="width: 100%; height: 260px;">
+							<li class="slide" data-enlargeurl="content/enlarged/1.jpg">
+								<img src="images/sale4.png"  alt="акция" title="акция" />
+							</li>
+						</ul>
+					</div>
+					
+					<h2><a href="contact.php">Забудьте о дискомфорте 100%</a></h2>
+					
+					<div class="post_meta">
+						<span class="entry_date">24 сентября - 12 октября 2019 года</span>
+					</div>
+					
+					<p>
+					При заказе двух пар стелек тм Foot Care, Вы получаете ПОДАРОК  "Наклейку на задник обуви" тм Foot Care*.
+					</p>
+					
+					<p>
+						<a class="button" href="contact.php">Узнать больше<span></span></a>
+					</p>
+				</div>
+
+			</div>
+			
+			<div class="clear"></div>
+		</div>
+	</div>
+<?php include "popular_goods.php"; ?>
+	
+	<?php include "footer.php"; ?>
+
+	
+	<!-- JAVASCRIPTS -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/dm3Slideshow.jquery.js"></script>
+	<script type="text/javascript" src="js/dm3FadeSlider.jquery.js"></script>
+	<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+	<script type="text/javascript" src="js/prettyPhoto/jquery.prettyPhoto.js"></script>
+	<script type="text/javascript" src="js/jquery.dmTabs.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
